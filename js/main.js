e7f36c33992e3b85c1c5a874caaf64a64a517a68
AvdalyanAnna <anna.avdalyan.2017@gmail.com>
2022-08-08 02:06:55 +0400

    changes

--- js/main.js
+++ js/main.js
@@ -557,6 +557,132 @@ $(document).ready(function () {
         readURL(this);
     });
 
+    function timer() {
+        var timer = 0;
+        const date = $('.order__spor-timer .time').attr('data-time')
+        const newDate = new Date(date).getTime()
+        const countdown = setInterval(() => {
+            timer = 1000;
+            const date = new Date().getTime(),
+                diff = newDate - date,
+                days = Math.floor(diff % (1000 * 60 * 60 * 24 * (365.25 / 12)) / (1000 * 60 * 60 * 24)),
+                hours = Math.floor(diff % (1000 * 60 * 60 * 24) / (1000 * 60 * 60)),
+                minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60)),
+                seconds = Math.floor((diff % (1000 * 60)) / 1000),
+                time = `${days < 10 ? '0' + days : days}:${hours < 10 ? '0' + hours : hours}:${minutes < 10 ? '0' + minutes : minutes}:${seconds < 10 ? '0' + seconds : seconds}`
+            $('.order__spor-timer .time').text(time)
+            if (diff === 0) {
+                clearInterval(countdown)
+            }
 
+        }, timer)
+    }
+
+    if ($('div').is('.order__spor-timer')) {
+        timer()
+    }
+
+    const ratings = document.querySelectorAll('.rating');
+    if (ratings.length > 0) {
+        initRatings();
+    }
+
+    function initRatings() {
+        let ratingActive, ratingValue;
+        // "Бегаем" по всем рейтингам на странице
+        for (let index = 0; index < ratings.length; index++) {
+            const rating = ratings[index];
+            initRating(rating);
+        }
+
+        // Инициализируем конкретный рейтинг
+        function initRating(rating) {
+            initRatingVars(rating);
+
+            setRatingActiveWidth();
+
+            if (rating.classList.contains('rating_set')) {
+                setRating(rating);
+            }
+        }
+
+        // Инициализайция переменных
+        function initRatingVars(rating) {
+            ratingActive = rating.querySelector('.rating__active');
+            ratingValue = rating.querySelector('.rating__value');
+        }
+        // Изменяем ширину активных звезд
+        function setRatingActiveWidth(index = ratingValue.innerHTML) {
+            const ratingActiveWidth = index / 0.05;
+            ratingActive.style.width = `${ratingActiveWidth}%`;
+        }
+        // Возможность указать оценку
+        function setRating(rating) {
+            const ratingItems = rating.querySelectorAll('.rating__item');
+            for (let index = 0; index < ratingItems.length; index++) {
+                const ratingItem = ratingItems[index];
+                ratingItem.addEventListener("mouseenter", function (e) {
+                    // Обновление переменных
+                    initRatingVars(rating);
+                    // Обновление активных звезд
+                    setRatingActiveWidth(ratingItem.value);
+                });
+                ratingItem.addEventListener("mouseleave", function (e) {
+                    // Обновление активных звезд
+                    setRatingActiveWidth();
+                });
+                ratingItem.addEventListener("click", function (e) {
+                    // Обновление переменных
+                    initRatingVars(rating);
+
+                    if (rating.dataset.ajax) {
+                        // "Отправить" на сервер
+                        setRatingValue(ratingItem.value, rating);
+                    } else {
+                        // Отобразить указанную оцнку
+                        ratingValue.innerHTML = index + 1;
+                        setRatingActiveWidth();
+                    }
+                });
+            }
+        }
+
+        async function setRatingValue(value, rating) {
+            if (!rating.classList.contains('rating_sending')) {
+                rating.classList.add('rating_sending');
+
+                // Отправика данных (value) на сервер
+                let response = await fetch('rating.json', {
+                    method: 'GET',
+
+                    //body: JSON.stringify({
+                    //	userRating: value
+                    //}),
+                    //headers: {
+                    //	'content-type': 'application/json'
+                    //}
+
+                });
+                if (response.ok) {
+                    const result = await response.json();
+
+                    // Получаем новый рейтинг
+                    const newRating = result.newRating;
+
+                    // Вывод нового среднего результата
+                    ratingValue.innerHTML = newRating;
+
+                    // Обновление активных звезд
+                    setRatingActiveWidth();
+
+                    rating.classList.remove('rating_sending');
+                } else {
+                    alert("Ошибка");
+
+                    rating.classList.remove('rating_sending');
+                }
+            }
+        }
+    }
 })
 
